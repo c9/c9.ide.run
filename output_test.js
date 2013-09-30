3e530407e46e781e5d47cc6a330f85c0fce50506
Fabian Jakobs <fabian@c9.io>
2013-09-30 09:03:43 +0000

    don't pass along the smith.io config. Instead pass the list of vfs servers

--- output_test.js
+++ output_test.js
@@ -64,13 +64,8 @@ require(["lib/architect/architect", "lib/chai/chai", "/vfs-root"],
         "plugins/c9.ide.ui/forms",
         "plugins/c9.fs/fs",
         "plugins/c9.fs/proc",
-        {
-            packagePath: "plugins/c9.vfs.client/vfs_client",
-            smithIo     : {
-                "path": "/smith.io/server"
-            }
-        },
-        "plugins/c9.vfs.client/endpoint.standalone",
+        "plugins/c9.vfs.client/vfs_client",
+        "plugins/c9.vfs.client/endpoint",
         "plugins/c9.ide.auth/auth",
         
         // Mock plugins
