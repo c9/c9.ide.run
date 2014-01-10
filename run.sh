e2e9aa47ea7341e4220e600ba3c3fd6f4fa16c9b
Ruben Daniels <ruben@c9.io>
2014-01-10 14:00:17 +0000

    Slightly better run.sh strategy

--- run.sh
+++ run.sh
@@ -21,9 +21,9 @@ fi
 # This is needed for 32 bit tmux
 export LD_LIBRARY_PATH=~/.c9/local/lib:$LD_LIBRARY_PATH
 
-# Kill any existing session
-$TMUX kill-session -t $NAME
-    
+# Rename session to prevent output to reconnect immediately
+$TMUX rename-session -t $NAME "$NAME"_old
+
 # Write the watch file
 echo "-1" > $WATCHFILE
 
@@ -37,6 +37,9 @@ $TMUX new -s $NAME "$CMD; ([ -e $WATCHFILE ] && rm $WATCHFILE)" \
     \; set-window-option -g aggressive-resize on \
     \; set-option -g prefix C-b \
     \; $DETACH
+
+# Kill any existing session
+$TMUX kill-session -t "$NAME"_old
     
 # Return the pid
 PID=`ps ax | grep $WATCHFILE | grep rm || echo -1`
