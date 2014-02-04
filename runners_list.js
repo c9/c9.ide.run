fe6197007feffd2b55461356f1d29f282a39ec1f
Lennart Kats <lclkats@gmail.com>
2014-02-04 18:05:00 +0100

    Show nice message for bad runner

--- runners_list.js
+++ runners_list.js
@@ -4,7 +4,12 @@ function readRunners(path) {
     var results = {};
     var runnersPath = __dirname + "/" + path + "/";
     fs.readdirSync(runnersPath).forEach(function (name) {
-        var json = JSON.parse(fs.readFileSync(runnersPath + name));
+    	try {
+        	var json = JSON.parse(fs.readFileSync(runnersPath + name));
+        } catch (e) {
+        	console.error("Syntax error in runner", runnersPath + name, e);
+        	throw e;
+        }
         json.caption = name.replace(/\.run$/, "");
         json.$builtin = true;
         results[json.caption] = json;
