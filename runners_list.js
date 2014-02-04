facc2342e9ad79699f6c2c098f6fba39571d5fd7
Lennart Kats <lclkats@gmail.com>
2014-02-04 17:21:37 +0100

    Don't use hosted runners on non-hosted and vice versa

--- runners_list.js
+++ runners_list.js
@@ -1,12 +1,17 @@
-var Fs = require("fs");
+var fs = require("fs");
 
-var runners = {};
-var runnersPath = __dirname + "/runners/";
-Fs.readdirSync(runnersPath).forEach(function (name) {
-    var json = JSON.parse(Fs.readFileSync(runnersPath + name));
-    json.caption = name.replace(/\.run$/, "");
-    json.$builtin = true;
-    runners[json.caption] = json;
-});
+function readRunners(path) {
+    var results = {};
+    var runnersPath = __dirname + "/" + path + "/";
+    fs.readdirSync(runnersPath).forEach(function (name) {
+        var json = JSON.parse(fs.readFileSync(runnersPath + name));
+        json.caption = name.replace(/\.run$/, "");
+        json.$builtin = true;
+        results[json.caption] = json;
+    });
+    return results;
+}
 
-module.exports = runners;
+module.exports.runners = readRunners("runners");
+
+module.exports.hostedRunners = readRunners("runners-hosted");
