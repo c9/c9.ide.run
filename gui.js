d1fc3ae1274f005e7be6aa8e95044c72a7908ff7
Ruben Daniels <ruben@c9.io>
2014-04-08 00:41:54 +0200

    Preferences

--- gui.js
+++ gui.js
@@ -432,7 +432,6 @@ define(function(require, module, exports) {
                 }];
                 
                 var container = hbox.$ext.appendChild(document.createElement("div"));
-                container.style.border = "1px solid rgb(37, 37, 37)";
                 container.style.width  = "600px";
                 container.style.marginBottom  = "30px";
                 
